dc101c1f8880367f8f77d398919ae862a7fdec36
Vanggie <jwang186@jhu.edu>
2023-02-13 23:58:06 -0800

    Delete addon.cc

--- addon.cc
+++
@@ -1,9 +0,0 @@
-//addon.cc
-#include <napi.h>
-#include "engine.h"
-
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return nertc::Engine::Init(env, exports);
-}
-
-NODE_API_MODULE(addon, InitAll)
